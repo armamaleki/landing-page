66783c2d870db9bec14058f4d68b60d345ee6854
arma.malekii <ArmaMamad1967>
2023-01-07 02:06:23 -0800

    add social coloms

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,3 +4,22 @@ $(document).ready(function(){
         autoWidth:true,
     });
 });
+
+$('.owl-carousel-2').owlCarousel({
+    // loop:true,
+    // rtl:true,
+    //
+    // // margin:10,
+    // nav:true,
+    // responsive:{
+    //     0:{
+    //         items:1
+    //     },
+    //     600:{
+    //         items:3
+    //     },
+    //     1000:{
+    //         items:5
+    //     }
+    // }
+})
